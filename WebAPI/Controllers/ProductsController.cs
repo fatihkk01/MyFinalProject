a0eee9fc40b9c7f19fdfb36997966ff78ed0ca8d
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-02-13 22:47:18 +0300

    13 Şubat 1.Commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    //Controller isimleri çoğul olur örn -> Products
+    //Attribute : Bir class ile ilgil bilgi verme ve imzalama yöntemidir.
+    //Route : Bu isteği yaparken insanların apiye nasıl ulaşacağını belirlediğimiz kısımdır.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosly coupled = Gevşek bağımlılık 
+        //Naming convention = İsimlendirme kuralı
+        //IoC Container = Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain = Bağımlılık zinciri
+            
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
